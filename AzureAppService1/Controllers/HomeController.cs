1355ba8422ab0b8a27ba48c23d1f48c55a60046f
tony23136463 <tony23136463@gmail.com>
2022-08-13 16:16:11 +0800

    first push

---
+++ AzureAppService1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AzureAppService1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using AzureAppService1.Models.ViewModel.Home;
+
+namespace AzureAppService1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(IndexViewModel view)
+        {
+            view.NumberC = view.NumberA + view.NumberB;
+
+            return View(view);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureAppService1/Models/ViewModel/Home/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureAppService1.Models.ViewModel.Home
+{
+    public class IndexViewModel
+    {
+        public decimal NumberA { get; set; }
+        public decimal NumberB { get; set; }
+        public decimal NumberC { get; set; }
+
+    }
+}
